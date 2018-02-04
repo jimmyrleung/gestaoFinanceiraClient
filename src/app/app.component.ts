db87886753731e44775f8996620ea9d36168c5a2
jimmyrl19 <jimmy.rl19@hotmail.com>
2018-02-03 22:30:24 -0200

    Semantic UI e dropdown de plano financeiro com dados fixos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { AfterViewInit, OnInit } from "@angular/core";
+//import * as $ from 'jquery';
+declare var jQuery: any; // It is imported globally on angular-cli.json, so we just need to declare it.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'app';
+  selectedvalue;
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    jQuery('.ui.dropdown').dropdown({
+      onChange: function (value, text, $selectedItem) {
+        this.selectedvalue = value;
+        console.log(this.selectedvalue);
+      }
+    });
+  }
 }
